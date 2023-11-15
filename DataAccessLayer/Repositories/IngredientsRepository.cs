b4d5d6e6cabf73feaa530f7c3af8c19c2bcb5eff
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-11-15 01:26:52 +0100

    Startup code for this chapter

---
+++ DataAccessLayer/Repositories/IngredientsRepository.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using DataAccessLayer.Contracts;
+using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class IngredientsRepository : IIngredientsRepository
+    {
+        public void AddIngredient(Ingredient ingredient)
+        {
+            string query = @"insert into Ingredients 
+                (Name, Weight, KcalPer100g, PricePer100g, Type) 
+                values (@Name, @Weight, @KcalPer100g, @PricePer100g, @Type)";
+
+            using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+            {
+                connection.Execute(query, ingredient);
+            }
+        }
+
+        public List<Ingredient> GetIngredients()
+        {
+            string query = "select * from Ingredients";
+
+            using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+            {
+                return connection.Query<Ingredient>(query).ToList();
+            }
+        }
+
+        public List<Ingredient> SearchIngredients(string name)
+        {
+            string query = $"select * from Ingredients where Name like '{name}%'";
+
+            using (IDbConnection connection = new SqlConnection(ConnectionHelper.ConnectionString))
+            {
+                return connection.Query<Ingredient>(query).ToList();
+            }
+        }
+    }
+}
